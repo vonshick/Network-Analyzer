8ce89af5754f26a8d57ad01477c1a36904b46c34
Vulwsztyn <artur.mostowski@student.put.poznan.pl>
2018-11-02 17:12:28 +0100

    JSON input is processed to Java classes

---
+++ src/main/java/pl/put/poznan/transformer/logic/Connection.java
@@ -0,0 +1,34 @@
+package pl.put.poznan.transformer.logic;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Connection {
+    @JsonProperty("from")
+    private int from;
+    @JsonProperty("to")
+    private int to;
+
+    public Connection() {
+    }
+
+    @JsonProperty("value")
+    private Double value;
+
+    public Connection(int from, int to, Double value) {
+        this.from = from;
+        this.to = to;
+        this.value = value;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+}
