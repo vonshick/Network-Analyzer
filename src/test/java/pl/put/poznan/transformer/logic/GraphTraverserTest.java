f4156342fb89fadd04e39ca446a96447c0bf3bf8
Vulwsztyn <artur.mostowski@student.put.poznan.pl>
2018-12-02 23:41:05 +0100

    Add tests

---
+++ src/test/java/pl/put/poznan/transformer/logic/GraphTraverserTest.java
@@ -0,0 +1,22 @@
+package pl.put.poznan.transformer.logic;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTraverserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void transform() {
+    }
+}
