be627eee327fa384a51dfbd3aabbd22297733c68
dominieq <dominik.szmyt@student.put.poznan.pl>
2018-12-15 15:57:15 +0100

    20 Unit Tests

--- src/test/java/pl/put/poznan/transformer/logic/BFSTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/BFSTest.java
@@ -8,15 +8,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BFSTest {
 
+    private BFS bfs;
+    private String simpleGraph;
+    private String oppositeConnection;
+
     @BeforeEach
     void setUp() {
+        bfs = new BFS(null);
+        simpleGraph = "{\"nodes\":[{\"id\":0,\"name\":\"Wezel 0\"," +
+                "\"type\":\"entry\",\"outgoing\":[{\"to\":1,\"from\":0,\"value\":1}],\"incoming\":[]},"
+                + "{\"id\":1,\"name\":\"Wezel 1\"," +
+                "\"type\":\"exit\",\"outgoing\":[],\"incoming\":[{\"to\":1,\"from\":0,\"value\":1}]}],"
+                + "\"connections\":[{\"to\":1,\"from\":0,\"value\":1}]}";
+
+        oppositeConnection = "{\"nodes\":[{\"id\":0,\"name\":\"Wezel 0\"," +
+                "\"type\":\"entry\",\"outgoing\":[{\"to\":0,\"from\":1,\"value\":1}],\"incoming\":[]},"
+                + "{\"id\":1,\"name\":\"Wezel 1\"," +
+                "\"type\":\"exit\",\"outgoing\":[],\"incoming\":[{\"to\":0,\"from\":1,\"value\":1}]}],"
+                + "\"connections\":[{\"to\":1,\"from\":0,\"value\":1}]}";
     }
 
     @AfterEach
     void tearDown() {
+        bfs = null;
+        simpleGraph = null;
+        oppositeConnection = null;
     }
 
     @Test
-    void traverse() {
+    void testTraverseSimpleGraph() {
+        bfs.setNetwork(simpleGraph);
+        assertTrue(bfs.traverse().getValue() == 1);
+    }
+
+    @Test
+    void testTraverseOppositeConnection() {
+        bfs.setNetwork(oppositeConnection);
+        try {
+            bfs.traverse().getValue();
+        } catch(Exception e) {
+            e.printStackTrace();
+            fail();
+        }
     }
 }
