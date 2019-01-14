98ddf0e87ee3a7383df6dc84e24778d3164d5a0b
dominieq <dominik.szmyt@student.put.poznan.pl>
2019-01-14 01:13:11 +0100

    6 method calls using mock objects

--- src/test/java/pl/put/poznan/transformer/logic/BFSTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/BFSTest.java
@@ -9,6 +9,7 @@ import pl.put.poznan.transformer.rest.GraphTraverserController;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class BFSTest {
 
@@ -16,6 +17,8 @@ class BFSTest {
     private String simpleGraph;
     private String oppositeConnection;
     private String mediumGraph;
+    private ArrayList<Double> mockIndexList;
+    private ArrayList<Integer> mockVisitedList;
 
     @BeforeEach
     void setUp() {
@@ -32,6 +35,8 @@ class BFSTest {
                 "\"type\":\"exit\",\"outgoing\":[],\"incoming\":[{\"to\":0,\"from\":1,\"value\":1}]}],"
                 + "\"connections\":[{\"to\":1,\"from\":0,\"value\":1}]}";
         mediumGraph = "{\"nodes\":[{\"id\":0,\"name\":\"Wezel 0\",\"type\":\"entry\",\"outgoing\":[{\"to\":4,\"from\":0,\"value\":1}],\"incoming\":[]},{\"id\":1,\"name\":\"Wezel 1\",\"type\":\"regular\",\"outgoing\":[{\"to\":3,\"from\":1,\"value\":8}],\"incoming\":[{\"to\":1,\"from\":6,\"value\":5},{\"to\":1,\"from\":7,\"value\":1}]},{\"id\":2,\"name\":\"Wezel 2\",\"type\":\"regular\",\"outgoing\":[{\"to\":5,\"from\":2,\"value\":6},{\"to\":9,\"from\":2,\"value\":8}],\"incoming\":[{\"to\":2,\"from\":5,\"value\":9},{\"to\":2,\"from\":8,\"value\":8}]},{\"id\":3,\"name\":\"Wezel 3\",\"type\":\"regular\",\"outgoing\":[{\"to\":4,\"from\":3,\"value\":9},{\"to\":8,\"from\":3,\"value\":4}],\"incoming\":[{\"to\":3,\"from\":1,\"value\":8},{\"to\":3,\"from\":6,\"value\":3}]},{\"id\":4,\"name\":\"Wezel 4\",\"type\":\"regular\",\"outgoing\":[{\"to\":6,\"from\":4,\"value\":9}],\"incoming\":[{\"to\":4,\"from\":0,\"value\":1},{\"to\":4,\"from\":3,\"value\":9}]},{\"id\":5,\"name\":\"Wezel 5\",\"type\":\"regular\",\"outgoing\":[{\"to\":2,\"from\":5,\"value\":9},{\"to\":7,\"from\":5,\"value\":5}],\"incoming\":[{\"to\":5,\"from\":2,\"value\":6}]},{\"id\":6,\"name\":\"Wezel 6\",\"type\":\"regular\",\"outgoing\":[{\"to\":1,\"from\":6,\"value\":5},{\"to\":3,\"from\":6,\"value\":3},{\"to\":8,\"from\":6,\"value\":7}],\"incoming\":[{\"to\":6,\"from\":4,\"value\":9}]},{\"id\":7,\"name\":\"Wezel 7\",\"type\":\"regular\",\"outgoing\":[{\"to\":1,\"from\":7,\"value\":1},{\"to\":9,\"from\":7,\"value\":2}],\"incoming\":[{\"to\":7,\"from\":5,\"value\":5},{\"to\":7,\"from\":9,\"value\":7}]},{\"id\":8,\"name\":\"Wezel 8\",\"type\":\"regular\",\"outgoing\":[{\"to\":2,\"from\":8,\"value\":8}],\"incoming\":[{\"to\":8,\"from\":3,\"value\":4},{\"to\":8,\"from\":6,\"value\":7}]},{\"id\":9,\"name\":\"Wezel 9\",\"type\":\"exit\",\"outgoing\":[{\"to\":7,\"from\":9,\"value\":7}],\"incoming\":[{\"to\":9,\"from\":2,\"value\":8},{\"to\":9,\"from\":7,\"value\":2}]}],\"connections\":[{\"to\":4,\"from\":0,\"value\":1},{\"to\":3,\"from\":1,\"value\":8},{\"to\":5,\"from\":2,\"value\":6},{\"to\":9,\"from\":2,\"value\":8},{\"to\":4,\"from\":3,\"value\":9},{\"to\":8,\"from\":3,\"value\":4},{\"to\":6,\"from\":4,\"value\":9},{\"to\":2,\"from\":5,\"value\":9},{\"to\":7,\"from\":5,\"value\":5},{\"to\":1,\"from\":6,\"value\":5},{\"to\":3,\"from\":6,\"value\":3},{\"to\":8,\"from\":6,\"value\":7},{\"to\":1,\"from\":7,\"value\":1},{\"to\":9,\"from\":7,\"value\":2},{\"to\":2,\"from\":8,\"value\":8},{\"to\":7,\"from\":9,\"value\":7}]}";
+        mockIndexList = mock(ArrayList.class);
+        mockVisitedList = mock(ArrayList.class);
     }
 
     @AfterEach
@@ -39,6 +44,8 @@ class BFSTest {
         bfs = null;
         simpleGraph = null;
         oppositeConnection = null;
+        mockIndexList = null;
+        mockVisitedList = null;
     }
 
     @Test
@@ -64,5 +71,18 @@ class BFSTest {
         lista.add(9);
         assertEquals(bfs.traverse().getVisitedList(),lista);
         assertEquals(bfs.traverse().getValue(),(Double) 33.0);
+    }
+
+    @Test
+    void testSimpleMocksInDFSRecursion() {
+        when(mockIndexList.get(0)).thenReturn(0.0);
+        when(mockIndexList.size()).thenReturn(1);
+        when(mockVisitedList.contains(0)).thenReturn(true);
+
+        bfs.setNetwork(simpleGraph);
+        bfs.argmin(mockIndexList, mockVisitedList);
+        verify(mockIndexList, times(2)).get(0);
+        verify(mockIndexList, times(2)).size();
+        verify(mockVisitedList, times(1)).contains(0);
     }
 }
